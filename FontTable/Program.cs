2c48134c477cae284c1e65812f569fc443b88ffa
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-05-16 12:28:58 -0400

    generating table of uppercase ASCII chars

--- FontTable/Program.cs
+++ FontTable/Program.cs
@@ -10,16 +10,16 @@ namespace FontTable
 {
     internal class Program
     {
-
-        public class FontTable
+        public class CharArr
         {
+            //  char array components
             string character;
             Font font;
             Bitmap bmp;
             Graphics g;
             int width;
             int height;
-            public FontTable(string toConvert, string fontName, int fontSize)
+            public CharArr(string toConvert, string fontName, int fontSize)
             {
                 character = toConvert;
                 font = new Font(fontName, fontSize, GraphicsUnit.Pixel);
@@ -32,37 +32,69 @@ namespace FontTable
                 g.Clear(Color.White);
                 g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixel;
             }
-            
-            public void testDraw()
+            public void drawChar()
             {
                 Console.WriteLine($"initiating draw of character {character} in font {font.Name}...");
                 g.DrawString(character, font, new SolidBrush(Color.Black), 0, 0);
 
-                for (int i = 0; i < width; i++)
+                for (int i = 0; i < height; i++)
                 {
-                    for (int j = 0; j < height; j++)
+                    for (int j = 0; j < width; j++)
                     {
-                        Color toPrint = bmp.GetPixel(i, j);
+                        Color toPrint = bmp.GetPixel(j, i);
                         Console.Write(toPrint.Name);
                     }
                     Console.Write("\n");
                 }
                 Console.WriteLine("...finished drawing character");
-                Console.WriteLine("-----------------");
 
                 g.Flush();
                 g.Dispose();
             }
         }
 
+        public class FontTable
+        {
+            // array of characters
+            public List<CharArr> table = new List<CharArr>();
+
+            //  font table components
+            public string fontName;
+            public int fontSize;
+            public FontTable(string name, int size)
+            {
+                fontName = name;
+                fontSize = size;
+            }
+
+            public void generateTable()
+            {
+                List<CharArr> tmpTable = new List<CharArr>();
+                for(var i = 65; i <= 90; i++)
+                {
+                    string toConvert = Convert.ToChar(i).ToString();
+                    CharArr toAdd = new CharArr(toConvert, fontName, fontSize);
+                    tmpTable.Add(toAdd);
+                }
+                table = tmpTable;
+            }
+            
+            public void printTable()
+            {
+                table.ForEach(character =>
+                {
+                    character.drawChar();
+                });
+            }
+            
+        }
+
         static void Main(string[] args)
         {
-            FontTable ft = new FontTable("H", "Arial", 12);
-            ft.testDraw();
-
-            FontTable ft2 = new FontTable("H", "Times New Roman", 12);
-            ft2.testDraw();
-
+            FontTable ft = new FontTable("Arial", 24);
+            ft.generateTable();
+            Console.WriteLine($"Generating font table of type {ft.fontName} and size {ft.fontSize}...");
+            ft.printTable();
 
             Console.ReadLine();
         }
