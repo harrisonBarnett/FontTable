7b5b7ee322949220fc6fa7f0f2dcc6de590c5b70
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-06-23 11:03:04 -0400

    add ticketparser class; notes

---
+++ FontTable/TicketParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* TODO //
+ * ticketparser 
+*/
+
+namespace FontTable
+{
+    public class TicketParser
+    {
+        public void Parse()
+        {
+            Console.WriteLine("parsing the ticket");
+        }
+    }
+}
--- FontTable/TicketRegion.cs
+++ FontTable/TicketRegion.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace FontTable
 {
-    internal class TicketRegion
+    public class TicketRegion
     {
+        #region Properties
+        public string RegionName;
+        public string FontFamily;
+        public int FontSize;
+        public bool IsBold;
+        public Dictionary<string, int> RegionArea;
+        public FontTable FontTable;
+        #endregion
+        public TicketRegion(string regionName, string fontFamily, int fontSize, bool isBold, int[] area, string bitMode)
+        {
+            RegionName = regionName;
+            FontFamily = fontFamily;
+            FontSize = fontSize;
+            IsBold = isBold;
+            RegionArea = ConvertToPixel(area);
+            FontTable = new FontTable(fontFamily, fontSize, bitMode);
+            FontTable.generateTable();
+        }
+        private Dictionary<string, int> ConvertToPixel(int[] area)
+        {
+            // TODO:
+            /* convert "points" of location of properties in ticket (i.e. Area="x,y,width,height") to pixel
+             * 72 points per inch
+             * 96 pixels per inch
+             * pixels = points / (72/96)?
+             */
+            return new Dictionary<string, int>();
+        }
     }
 }
